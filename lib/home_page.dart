0cb8a1943102a130aa6d2d432202e90c73bb2b28
Hitbee <audtndkdlel@gmail.com>
2020-10-29 04:23:55 +0900

    Code Clean Up

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,7 +12,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool taf = true;
+  int gchcount = 0;
+  int sochcount = 0;
+  String gch = "assets/images/gch1.png";
+  String soch = "assets/images/soch1.png";
 
   @override
   Widget build(BuildContext context) {
@@ -37,29 +42,56 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Padding(
           padding: const EdgeInsets.only(top: 250),
-          child:
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                  Container(
-                    height: 150.0,
-                    width: 200,
-                    child: IconButton(
-                      icon: Image.asset("assets/images/gch1.png"),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Container(
-                    height: 150.0,
-                    width: 200,
-                    child: IconButton(
-                      icon: Image.asset("assets/images/soch1.png"),
-                      onPressed: () {},
-              ),
-            ),
-          ]),
+          child: _cheerbutton(),
         ),
       ),
     );
+  }
+
+  Row _cheerbutton() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Container(
+        height: 150.0,
+        width: 200,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30),
+          child: IconButton(
+            icon: Image.asset(gch),
+            onPressed: () {
+              setState(() {
+                if (gchcount == 0) {
+                  gch = "assets/images/gch2.png";
+                  gchcount++;
+                } else {
+                  gch = "assets/images/gch1.png";
+                  gchcount--;
+                }
+              });
+            },
+          ),
+        ),
+      ),
+      Container(
+        height: 150.0,
+        width: 200,
+        child: Padding(
+          padding: const EdgeInsets.only(right: 30),
+          child: IconButton(
+            icon: Image.asset(soch),
+            onPressed: () {
+              setState(() {
+                if (sochcount == 0) {
+                  soch = "assets/images/soch2.png";
+                  sochcount++;
+                } else {
+                  soch = "assets/images/soch1.png";
+                  sochcount--;
+                }
+              });
+            },
+          ),
+        ),
+      ),
+        ]);
   }
 }
