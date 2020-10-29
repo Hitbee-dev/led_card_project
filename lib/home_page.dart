dd20c94823c446325b5de7fdb1c3e5dba0ce92dc
Hitbee <audtndkdlel@gmail.com>
2020-10-29 16:32:30 +0900

    Home화면 Layout 종료

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,8 +14,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int gchcount = 0;
   int sochcount = 0;
+  int exitcount = 0;
   String gch = "assets/images/gch1.png";
   String soch = "assets/images/soch1.png";
+  String exit = "assets/images/exit1.png";
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +44,14 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Padding(
           padding: const EdgeInsets.only(top: 250),
-          child: _cheerbutton(),
+          child: Column(
+            children: [
+              _cheerbutton(),
+               _exitbutton(),
+              Expanded(flex: 1, child: Container()), // 맨 밑에 회사 정보를 표기하기 위한 빈 공간 채우기
+              _companyinfo(),
+            ],
+          )
         ),
       ),
     );
@@ -51,10 +60,10 @@ class _HomePageState extends State<HomePage> {
   Row _cheerbutton() {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
       Container(
-        height: 150.0,
-        width: 200,
+        height: 130,
+        width: 180,
         child: Padding(
-          padding: const EdgeInsets.only(left: 30),
+          padding: const EdgeInsets.only(left: 40),
           child: IconButton(
             icon: Image.asset(gch),
             onPressed: () {
@@ -72,10 +81,10 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       Container(
-        height: 150.0,
-        width: 200,
+        height: 130,
+        width: 180,
         child: Padding(
-          padding: const EdgeInsets.only(right: 30),
+          padding: const EdgeInsets.only(right: 40),
           child: IconButton(
             icon: Image.asset(soch),
             onPressed: () {
@@ -92,6 +101,43 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-        ]);
+    ]);
+  }
+
+  Row _exitbutton() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      Container(
+        height: 80,
+        width: 270,
+          child: IconButton(
+            icon: Image.asset(exit),
+            onPressed: () {
+              setState(() {
+                if (exitcount == 0) {
+                  exit = "assets/images/exit2.png";
+                  exitcount++;
+                } else {
+                  exit = "assets/images/exit1.png";
+                  exitcount--;
+                }
+              });
+            },
+          ),
+        ),
+    ]);
+  }
+
+  Widget _companyinfo() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Text("(주)코포워드", style: TextStyle(color: Colors.white, fontSize: 11)),
+              Text("대전광역시 서구 월평로 85 학산빌딩 415호", style: TextStyle(color: Colors.white, fontSize: 11)),
+              Text("dev@cofoward.com", style: TextStyle(color: Colors.white, fontSize: 11)),
+            ],
+      ),
+    );
   }
 }
