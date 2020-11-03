bcc24b80bab9536213516a2e7a85439a40abde69
Hitbee <audtndkdlel@gmail.com>
2020-11-03 17:22:38 +0900

    BackButton Event Add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,6 +11,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+  static DateTime currentBackPressTime;
+
+  _isEnd() {
+    DateTime now = DateTime.now();
+    if(currentBackPressTime == null || now.difference(currentBackPressTime) > Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      _globalKey.currentState..hideCurrentSnackBar()..showSnackBar(
+        SnackBar(
+          duration: Duration(seconds: 2),
+          content: Text("종료하시려면 한번 더 누르세요."),
+        ));
+      return false;
+    }
+    return true;
+  }
+  /// BackButton 연속 2번 누를 시 종료 Event
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
@@ -18,8 +36,15 @@ class _HomePageState extends State<HomePage> {
 
     if (size == null) size = MediaQuery.of(context).size;
     /// 상태바 색상 변경
-    return Scaffold(
-      body: MenuPage(),
+    return WillPopScope(
+      onWillPop: () async {
+        bool result = _isEnd();
+        return await Future.value(result);
+      },
+      child: Scaffold(
+        key: _globalKey,
+        body: MenuPage(),
+      ),
     );
   }
 }
